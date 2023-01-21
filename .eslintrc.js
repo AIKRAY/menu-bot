cce9b253ed9626d850e4fe8cc4587f2fb71ae731
Roman Sluka <r.sluka@itransition.com>
2023-01-21 16:29:16 +0400

    Added necessary basic libraries.

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+module.exports = {
+  env: {
+    es2020: true,
+    jest: true,
+    node: true,
+    browser: true,
+  },
+  extends: ['react-app', 'airbnb-typescript', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['simple-import-sort', 'prettier', 'prefer-arrow'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'react/jsx-key': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/prefer-readonly': 'error',
+    '@typescript-eslint/member-ordering': 'error',
+    'prefer-arrow/prefer-arrow-functions': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'arrow-body-style': ['error', 'as-needed'],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'react/no-danger': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+    ],
+    'react/require-default-props': [
+      'error',
+      { forbidDefaultForRequired: true, ignoreFunctionalComponents: true },
+    ],
+    '@typescript-eslint/lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+    'jsx-a11y/label-has-associated-control': [2, { assert: 'either' }],
+    'jsx-a11y/interactive-supports-focus': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'no-plusplus': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  htmlWhitespaceSensitivity: 'ignore',
+  endOfLine: 'auto',
+  arrowParens: 'avoid',
+};
